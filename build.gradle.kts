57eda30577388d0443e77eed6ad58483b034a77b
Artur <arturmineev9@mail.ru>
2026-04-13 12:28:35 +0300

    configured detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,32 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+
 plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.detekt)
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.serialization) apply false
 }
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    configure<DetektExtension> {
+        config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+        buildUponDefaultConfig = true
+        autoCorrect = true
+        parallel = true
+    }
+
+    dependencies {
+        "detektPlugins"(rootProject.libs.detekt.formatting)
+    }
+}
+
 private val versionMajor = 1
 private val versionMinor = 0
-
 val versionName by extra(initialValue = "$versionMajor.$versionMinor")
 val versionCode by extra(initialValue = versionMajor * 1000 + versionMinor * 10)
